7a938dc63c116d905c3adddf3ccc3bc53c20ff6b
Stone Yao <syao@uwaterloo.ca>
2025-11-13 22:44:27 -0500

    hot fix

--- src/main/engine.cpp
+++ src/main/engine.cpp
@@ -1,9 +1,17 @@
 #include "board.h"
+#include <iostream>
+
+using namespace std;
 
 int main() {
     Board b = {};
     setup_startpos(b);
     print_board(b);
+
+    if (!assert_bb_consistency(b)) {
+        cout << "bit board error!" << endl;
+        return -1;
+    }
 
     return 0;
 }
